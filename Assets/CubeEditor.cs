9dfe92e39b92ffcae8ba6336cd84343cc5c7b845
Isfand <isfandyar.ali@me.com>
2020-10-18 11:56:13 +0100

    Used a serializable field to reference the top label Text Mesh component rather then find in children as it is a bit expensive

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -12,6 +12,7 @@ public class CubeEditor : MonoBehaviour
     [Range(1.0f, 20.0f)]
     private float _gridSize = 10.0f;
 
+    [SerializeField]
     private TextMesh _textMesh = null;
 
     /// <summary>
@@ -26,7 +27,7 @@ public class CubeEditor : MonoBehaviour
     /// <summary>
     /// Snaps GameObjects to a gridsize, for example a 10 gridsize;  You move 6 units in x, divided this by 10
     /// which is 0.6, rounded to the nearest int is 1 then multiply by 10 giving you 10 and setting its position.
-    /// Then it Sets Label text on Top Quad by finding the first TextMesh it finds on the GameObject and dividing by the grid size to get 0,1 etc
+    /// Then it Sets the Label text on the Top Quad and dividing by the grid size to get 0,1 etc
     /// </summary>
     private void SnapToGrid(float gridSize)
     {
@@ -36,8 +37,9 @@ public class CubeEditor : MonoBehaviour
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
 
-        // TODO Use alt method to find this TextMesh reference in child (if it breaks).
-        _textMesh = GetComponentInChildren<TextMesh>();
-        _textMesh.text = string.Format("{0},{1}", snapPos.x / gridSize, snapPos.z/ gridSize);
+        if (_textMesh != null)
+        {
+            _textMesh.text = string.Format("{0},{1}", snapPos.x / gridSize, snapPos.z / gridSize);
+        }
     }
 }
