66ee33ff3397345d67844885b58250036049ab73
Isfand <isfandyar.ali@me.com>
2020-10-18 11:20:23 +0100

    Added a editor script that allows us to snap cudes or any gameObject to a specified grid sizethat doesn't use Unity version of Snaping via the menu

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+
+    [SerializeField]
+    [Range(1.0f, 20.0f)]
+    private float _gridSize = 10.0f;
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Update()
+    {
+        SnapToGrid(_gridSize);
+    }
+
+
+    /// <summary>
+    /// Snaps GameObjects to a gridsize, for example a 10 gridsize;  You move 6 units in x, divided this by 10
+    /// which is 0.6, rounded to the nearest int is 1 then multiply by 10 giving you 10.
+    /// </summary>
+    private void SnapToGrid(float gridSize)
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
