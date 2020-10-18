053b7ab8d9afd16a72568337b361756b891f8ac9
Isfand <isfandyar.ali@me.com>
2020-10-18 11:52:47 +0100

    Cleaned up assets folders, setup labels using text mesh on the top quads of the cube so that it automcatically sets it laebl text to grid based format so 0,1 and 0,2 and so on

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
 
     [SerializeField]
     [Range(1.0f, 20.0f)]
     private float _gridSize = 10.0f;
 
+    private TextMesh _textMesh = null;
 
     /// <summary>
     /// 
@@ -23,7 +25,8 @@ public class EditorSnap : MonoBehaviour
 
     /// <summary>
     /// Snaps GameObjects to a gridsize, for example a 10 gridsize;  You move 6 units in x, divided this by 10
-    /// which is 0.6, rounded to the nearest int is 1 then multiply by 10 giving you 10.
+    /// which is 0.6, rounded to the nearest int is 1 then multiply by 10 giving you 10 and setting its position.
+    /// Then it Sets Label text on Top Quad by finding the first TextMesh it finds on the GameObject and dividing by the grid size to get 0,1 etc
     /// </summary>
     private void SnapToGrid(float gridSize)
     {
@@ -32,5 +35,9 @@ public class EditorSnap : MonoBehaviour
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        // TODO Use alt method to find this TextMesh reference in child (if it breaks).
+        _textMesh = GetComponentInChildren<TextMesh>();
+        _textMesh.text = string.Format("{0},{1}", snapPos.x / gridSize, snapPos.z/ gridSize);
     }
 }
